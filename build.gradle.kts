3a00b06ebf227c9e1c77a01408ef70c312ac1c52
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-22 16:09:12 +0200

    Lagt til spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
     kotlin("jvm") version "1.9.0"
 
     id("com.diffplug.spotless") version "6.20.0"
+
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("plugin.spring") version "1.9.0"
 }
 
 repositories {
@@ -16,6 +20,21 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(kotlin("stdlib"))
+
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 java {
@@ -37,10 +56,16 @@ spotless {
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = javaVersion.toString()
+        kotlinOptions {
+            jvmTarget = javaVersion.toString()
+            freeCompilerArgs += "-Xjsr305=strict"
+        }
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = javaVersion.toString()
-        kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+        kotlinOptions {
+            jvmTarget = javaVersion.toString()
+            freeCompilerArgs += "-Xjsr305=strict"
+            freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+        }
     }
 }
