c02ca0f6d9b9e1ea02edd18b85fe4e732328d372
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-22 12:50:24 +0200

    Lagt til gradle og ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 val javaVersion = JavaVersion.VERSION_17
 
 plugins {
+    application
+
     kotlin("jvm") version "1.9.0"
 
-    application
+    id("com.diffplug.spotless") version "6.20.0"
 }
 
 repositories {
@@ -11,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib"))
 }
 
 java {
@@ -22,5 +27,20 @@ application {
     mainClass.set("no.nav.tilleggsstonader.soknad.AppKt")
 }
 
+apply(plugin = "com.diffplug.spotless")
+
+spotless {
+    kotlin {
+        ktlint("0.50.0")
+    }
+}
+
 tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+        kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
 }
