61c90054ff672714248474ecf88c9bd741fe9c82
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-24 12:32:27 +0200

    Legger til securelogs

--- src/main/kotlin/no/nav/tilleggsstonader/soknad/infrastruktur/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/infrastruktur/config/ApplicationConfig.kt
@@ -1,6 +1,12 @@
 package no.nav.tilleggsstonader.soknad.infrastruktur.config
 
+import no.nav.tilleggsstonader.soknad.infrastruktur.config.SecureLogger.secureLogger
 import org.springframework.boot.SpringBootConfiguration
 
 @SpringBootConfiguration
-class ApplicationConfig
+class ApplicationConfig {
+
+    init {
+        secureLogger.info("securelogger test")
+    }
+}
