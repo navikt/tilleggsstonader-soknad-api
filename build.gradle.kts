17762e450bb495e57493f8fe922a1269b69da158
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-25 12:36:03 +0200

    Gradleforbedringer (#6)
    * Forenklet build.gradle med å bruke jvmToolChain
    * slettet unødvendig stdlib som tas in automatisk med plugin
    
    * dependency resolutionStrategy
    * Skal ikke tillate at deps endrer seg mellom build og publish
    * Skal ikke tillate snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
-val javaVersion = JavaVersion.VERSION_17
+val javaVersion = JavaLanguageVersion.of(17)
 
 group = "no.nav.tilleggsstonader.soknad"
 version = "1.0.0"
@@ -26,11 +26,13 @@ configure<KtlintExtension> {
     version.set("0.50.0")
 }
 
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation(kotlin("stdlib"))
+configurations.all {
+    resolutionStrategy {
+        failOnNonReproducibleResolution()
+    }
+}
 
+dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -46,38 +48,26 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-java {
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
+kotlin {
+    jvmToolchain(javaVersion.asInt())
+
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+    }
 }
 
 application {
     mainClass.set("no.nav.tilleggsstonader.soknad.AppKt")
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-            freeCompilerArgs += "-Xjsr305=strict"
-        }
-    }
-    compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-            freeCompilerArgs += "-Xjsr305=strict"
-            freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
-        }
-    }
-    test {
-        useJUnitPlatform()
-    }
-}
-
 if (project.hasProperty("skipLint")) {
     gradle.startParameter.excludedTaskNames += "ktlintMainSourceSetCheck"
 }
 
-tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    this.archiveFileName.set("app.jar")
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
 }
