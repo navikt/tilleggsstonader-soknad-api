32c33a936637e437bdec4537156ca2fd37279dd2
eirarset <eirik.arseth@nav.no>
2023-10-27 00:04:07 +0200

    Når søknaden er arkivert skal det opprettes en task å sende søknaden/journalposten til sak for håndtering

--- src/main/kotlin/no/nav/tilleggsstonader/soknad/prosessering/ArkiverSøknadTask.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/prosessering/ArkiverSøknadTask.kt
@@ -3,6 +3,7 @@ package no.nav.tilleggsstonader.soknad.prosessering
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.internal.TaskService
 import no.nav.tilleggsstonader.soknad.arkivering.ArkiveringService
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -11,11 +12,16 @@ import java.util.UUID
 @TaskStepBeskrivelse(taskStepType = ArkiverSøknadTask.TYPE, beskrivelse = "Arkiver søknad")
 class ArkiverSøknadTask(
     private val arkiveringService: ArkiveringService,
+    private val taskService: TaskService,
 ) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
         val søknadId = UUID.fromString(task.payload)
         arkiveringService.journalførSøknad(søknadId, task.callId)
+    }
+
+    override fun onCompletion(task: Task) {
+        taskService.save(SendTilSaksbehandlingTask.opprettTask(task))
     }
 
     companion object {
