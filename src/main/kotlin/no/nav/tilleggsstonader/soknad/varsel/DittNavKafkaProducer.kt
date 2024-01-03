e0e8a172a4b1b702cdfcba2be0cdb04108da9ce1
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2024-01-03 13:17:26 +0100

    Integrsjon mot kafka topic dittNav og funskjnalitet for ås ende meldinger

---
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/varsel/DittNavKafkaProducer.kt
@@ -0,0 +1,65 @@
+package no.nav.tilleggsstonader.soknad.varsel
+
+import no.nav.brukernotifikasjon.schemas.builders.BeskjedInputBuilder
+import no.nav.brukernotifikasjon.schemas.builders.NokkelInputBuilder
+import no.nav.brukernotifikasjon.schemas.builders.domain.PreferertKanal
+import no.nav.brukernotifikasjon.schemas.input.BeskjedInput
+import no.nav.brukernotifikasjon.schemas.input.NokkelInput
+import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+import java.net.URL
+import java.time.LocalDateTime
+import java.time.ZoneOffset.UTC
+
+@Service
+class DittNavKafkaProducer(private val kafkaTemplate: KafkaTemplate<NokkelInput, BeskjedInput>) {
+
+    @Value("\${KAFKA_TOPIC_DITTNAV}")
+    private lateinit var topic: String
+
+    fun sendToKafka(
+        fnr: String,
+        melding: String,
+        grupperingsnummer: String,
+        eventId: String,
+        link: URL? = null,
+        kanal: PreferertKanal? = null,
+    ) {
+        val nokkel = lagNøkkel(fnr, grupperingsnummer, eventId)
+        val beskjed = lagBeskjed(melding, link, kanal)
+
+        secureLogger.debug("Sending to Kafka topic: {}: {}", topic, beskjed)
+        runCatching {
+            val producerRecord = ProducerRecord(topic, nokkel, beskjed)
+            kafkaTemplate.send(producerRecord).get()
+        }.onFailure {
+            val errorMessage = "Could not send DittNav to Kafka. Check secure logs for more information."
+            secureLogger.error("Could not send DittNav to Kafka melding={}", beskjed, it)
+            throw RuntimeException(errorMessage)
+        }
+    }
+    private fun lagNøkkel(fnr: String, grupperingsId: String, eventId: String): NokkelInput =
+        NokkelInputBuilder()
+            .withAppnavn("tilleggsstonader-soknad-api")
+            .withNamespace("tilleggsstonader")
+            .withFodselsnummer(fnr)
+            .withGrupperingsId(grupperingsId)
+            .withEventId(eventId)
+            .build()
+
+    private fun lagBeskjed(melding: String, link: URL?, kanal: PreferertKanal?): BeskjedInput {
+        val builder = BeskjedInputBuilder()
+            .withSikkerhetsnivaa(4)
+            .withSynligFremTil(null)
+            .withTekst(melding)
+            .withTidspunkt(LocalDateTime.now(UTC))
+
+        if (link != null) builder.withLink(link)
+        if (kanal != null) builder.withEksternVarsling(true).withPrefererteKanaler(kanal)
+
+        return builder.build()
+    }
+}
