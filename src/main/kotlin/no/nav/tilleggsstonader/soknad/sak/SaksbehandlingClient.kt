fbc83d8d04d21aa5b074f514133f2b0cb2748241
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-29 08:09:33 +0100

    Oppdatert til nytt objekt for å kalle på saksbehandling for å behandl… (#90)

--- src/main/kotlin/no/nav/tilleggsstonader/soknad/sak/SaksbehandlingClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/sak/SaksbehandlingClient.kt
@@ -1,7 +1,7 @@
 package no.nav.tilleggsstonader.soknad.sak
 
 import no.nav.tilleggsstonader.kontrakter.felles.IdentStønadstype
-import no.nav.tilleggsstonader.kontrakter.sak.journalføring.AutomatiskJournalføringRequest
+import no.nav.tilleggsstonader.kontrakter.sak.journalføring.HåndterSøknadRequest
 import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -19,7 +19,7 @@ class SaksbehandlingClient(
 
     private val sakUri = UriComponentsBuilder.fromUri(uri).pathSegment("api", "ekstern").build()
 
-    fun sendTilSak(request: AutomatiskJournalføringRequest) {
+    fun sendTilSak(request: HåndterSøknadRequest) {
         val uri = UriComponentsBuilder.fromUri(sakUri.toUri())
             .pathSegment("handter-soknad").build().toUriString()
         postForEntityNullable<Void>(uri, request)
