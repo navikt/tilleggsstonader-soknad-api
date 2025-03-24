1dc3bec4700ad7e8ad6a11460f6c71b9015a0068
Johan Blomgren <blommish@users.noreply.github.com>
2025-03-24 09:07:30 +0100

    Oppdatert avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 val javaVersion = JavaLanguageVersion.of(21)
-val tilleggsstønaderLibsVersion = "2025.03.14-14.38.d650c79601e9"
-val tilleggsstønaderKontrakterVersion = "2025.03.17-10.34.19d9ad1090ff"
-val familieProsesseringVersion = "2.20241011144712_deb1f2c"
-val tokenSupportVersion = "5.0.8"
+val tilleggsstønaderLibsVersion = "2025.03.24-08.48.93a92ef744b9"
+val tilleggsstønaderKontrakterVersion = "2025.03.24-08.34.c5f84c4f3794"
+val familieProsesseringVersion = "2.20250317161652_fe22655"
+val tokenSupportVersion = "5.0.20"
 val wiremockVersion = "3.0.1"
-val testcontainerVersion = "1.20.3"
+val testcontainerVersion = "1.20.6"
 
 group = "no.nav.tilleggsstonader.soknad"
 version = "1.0.0"
@@ -12,16 +12,16 @@ version = "1.0.0"
 plugins {
     application
 
-    kotlin("jvm") version "2.0.21"
-    id("com.diffplug.spotless") version "7.0.1"
-    id("com.github.ben-manes.versions") version "0.51.0"
+    kotlin("jvm") version "2.1.20"
+    id("com.diffplug.spotless") version "7.0.2"
+    id("com.github.ben-manes.versions") version "0.52.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
 
-    id("org.springframework.boot") version "3.3.5"
-    id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.spring") version "2.0.21"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.spring") version "2.1.20"
 
-    id("org.cyclonedx.bom") version "1.10.0"
+    id("org.cyclonedx.bom") version "2.2.0"
 }
 
 repositories {
@@ -77,12 +77,12 @@ dependencies {
 
     // Kafka
     implementation("org.springframework.kafka:spring-kafka")
-    implementation("no.nav.tms.varsel:kotlin-builder:2.0.0")
+    implementation("no.nav.tms.varsel:kotlin-builder:2.1.1")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:$wiremockVersion")
-    testImplementation("io.mockk:mockk:1.13.13")
+    testImplementation("io.mockk:mockk:1.13.17")
 
     testImplementation("org.testcontainers:postgresql:$testcontainerVersion")
 
