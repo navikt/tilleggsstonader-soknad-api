ae6b52d60330dcfe4ddf27e14a5044713ae3ad77
Sigmund Berbom <sberbom@gmail.com>
2025-09-25 17:05:24 +0200

    🤡 Lag endepunkt som mocker mottak av kjøreliste

--- src/main/kotlin/no/nav/tilleggsstonader/soknad/kjøreliste/KjørelisteController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/kjøreliste/KjørelisteController.kt
@@ -5,8 +5,12 @@ import no.nav.tilleggsstonader.libs.sikkerhet.EksternBrukerUtils
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("api/kjorelister")
@@ -20,4 +24,13 @@ class KjørelisteController {
     fun hentRammevedtakDetaljer(
         @PathVariable rammevedtakId: String,
     ): RammevedtakDto? = rammevedtakDtoMock.find { rammevedtak -> rammevedtak.id == rammevedtakId }
+
+    @PostMapping()
+    fun mottaKjørelister(
+        @RequestBody request: Kjøreliste,
+    ): KjørelisteResponse {
+        val mottattTidspunkt = LocalDateTime.now()
+        val saksnummer = Random.nextInt(1000, 10000)
+        return KjørelisteResponse(mottattTidspunkt, saksnummer)
+    }
 }
