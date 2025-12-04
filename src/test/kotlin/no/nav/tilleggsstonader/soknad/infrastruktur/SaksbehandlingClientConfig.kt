1b3a5b098037025c9f1e466eb21ebd9660b523e4
Runar Skagestad <runar.skagestad@nav.no>
2025-12-04 14:53:39 +0100

    🤡 Mock respons til `harBehandlingUnderArbeid`

--- src/test/kotlin/no/nav/tilleggsstonader/soknad/infrastruktur/SaksbehandlingClientConfig.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/soknad/infrastruktur/SaksbehandlingClientConfig.kt
@@ -14,6 +14,7 @@ class SaksbehandlingClientConfig {
     fun saksbehandlingClient(): SaksbehandlingClient {
         val client = mockk<SaksbehandlingClient>()
         every { client.skalRoutesTilNyLøsning(any()) } returns true
+        every { client.harBehandlingUnderArbeid(any()) } returns false
         return client
     }
 }
