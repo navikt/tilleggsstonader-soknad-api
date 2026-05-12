7eb667d236fc9f71499d8fc0c2674dab524c5cc3
Runar Skagestad <runar.skagestad@nav.no>
2026-05-12 09:56:00 +0200

    ⬆️ Oppgrader kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,14 @@ version = "1.0.0"
 plugins {
     application
 
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.21"
     id("com.diffplug.spotless") version "8.4.0"
     id("com.github.ben-manes.versions") version "0.54.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("plugin.spring") version "2.3.21"
 }
 
 repositories {
