469f602c7abdfeaf3348ca5356cd0b258d079ef0
Andreas Strand <andreas.strand@nav.no>
2026-08-03 22:32:04 +0200

    Endrer prefix på versions-plugin da den har blitt endret og logger warning, samt bump versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
     kotlin("jvm") version "2.3.21"
     id("com.diffplug.spotless") version "8.8.0"
-    id("com.github.ben-manes.versions") version "0.55.0"
+    id("io.github.ben-manes.versions") version "0.59.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 
     id("org.springframework.boot") version "4.1.0"
