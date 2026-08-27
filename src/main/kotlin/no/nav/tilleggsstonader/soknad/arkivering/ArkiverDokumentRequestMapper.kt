4a58e2c02bc0342721fa0af305290ee495996550
Andreas Strand <andreas.strand@nav.no>
2026-08-27 13:35:00 +0200

    Oppdaterer kontrakter-versjon hvor nye stønadstyper for støtte til reise ved oppstart, avslutning og hjemreise er lagt til. Legger til nødvendige brancher i when-statements og todo'er for at det skal kompilere (#329)

--- src/main/kotlin/no/nav/tilleggsstonader/soknad/arkivering/ArkiverDokumentRequestMapper.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/soknad/arkivering/ArkiverDokumentRequestMapper.kt
@@ -65,6 +65,9 @@ object ArkiverDokumentRequestMapper {
 
             // TODO: Implementer arkivering for SØKNAD_FLYTTING med korrekt stønadstype (TSO/TSR)
             Skjematype.SØKNAD_FLYTTING -> error("Håndterer ikke skjema $type")
+
+            // TODO: Implementer arkivering for SØKNAD_STØTTE_TIL_REISE_OPPSTART_AVSLUTNING_HJEMREISE med korrekt stønadstype (TSO/TSR)
+            Skjematype.SØKNAD_STØTTE_TIL_REISE_OPPSTART_AVSLUTNING_HJEMREISE -> error("Håndterer ikke skjema $type")
         }
 
     private fun typeVedlegg(
@@ -88,6 +91,9 @@ object ArkiverDokumentRequestMapper {
 
             // TODO: Implementer arkivering av vedlegg for SØKNAD_FLYTTING med korrekt stønadstype (TSO/TSR)
             Skjematype.SØKNAD_FLYTTING -> error("Håndterer ikke skjema $type")
+
+            // TODO: Implementer arkivering av vedlegg for SØKNAD_STØTTE_TIL_REISE_OPPSTART_AVSLUTNING_HJEMREISE med korrekt stønadstype (TSO/TSR)
+            Skjematype.SØKNAD_STØTTE_TIL_REISE_OPPSTART_AVSLUTNING_HJEMREISE -> error("Håndterer ikke skjema $type")
         }
 
     private fun mapVedlegg(
